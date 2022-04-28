20ead95fc6225d6286b440a0de5aca0ef64ac850
Julio <juliobin.info@gmail.com>
2022-04-28 15:27:54 -0300

    curso react rocketseat

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]); //sempre que for usar um state que for armazenar uma lista ..iniciar com array vazio
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-List">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
